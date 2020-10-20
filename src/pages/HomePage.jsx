28552f6ae7e64876033208b86fd3bab7046821cb
TabithaSwart-Neill <tabithaswart@icloud.com>
2020-10-20 13:27:41 +1000

    GettingDatafromtheAPI
    GettingDatafromtheAPI
    Step 1 and 2 in mentor guide

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { allProjects } from "../data";
 import ProjectCard from "../components/ProjectCard/ProjectCard";
 
 function HomePage() {
+    const [projectList, setProjectList] = useState([]);
+
+    useEffect(() => {
+        setProjectList(allProjects);
+    }, []);
+
+
     return (
         <div id="project-list">
-            {allProjects.map((projectData, key) => {
+            {projectList.map((projectData, key) => {
                 return <ProjectCard key={key} projectData={projectData} />;
                 // <div key={key}>{projectData.title}</div>;
                 
